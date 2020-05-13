6a4c0886b946dd4c63938b81d00f5f216591c8d6
victor47n <victor47n@gmail.com>
2020-05-13 20:08:34 -0300

    Inicio do projeto com a muie andando e os homi correndo atras

---
+++ Assets/Scripts/ControllerInimigo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ControllerInimigo : MonoBehaviour
+{
+    public GameObject jogador;
+    public float velocidade = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+
+        /* Inimigo só se move se estiver a uma certa distancia do jogador */
+        if(distancia > 3)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+            /* Inimigo se move na direção do jogador */
+            Vector3 direcao = jogador.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+
+            /* Inimigo rotaciona para o jogador */
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+}
---
+++ Assets/Scripts/ControllerJogador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ControllerJogador : MonoBehaviour
+{
+    /* Velocidade dos personagens */
+    public float velocidade = 2;
+    Vector3 direcao;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        } 
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+
+    /* FixedUpdate roda em um tempo fixo e nao em todo frame do jogo como o Update*/
+    void FixedUpdate()
+    {
+        /* Pegando o Rigidbody e movendo a posição que o Rigidbody já está + a posição que eu quero ir */
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * velocidade * Time.deltaTime));
+    }
+}
